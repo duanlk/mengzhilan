2eb6bb30e9d8186e199bf671b9482946cb3e3e97
duanlinkang <you@example.com>
2021-02-22 17:20:03 +0800

    项目搭建

---
+++ src/utils/DBUtils.java
@@ -0,0 +1,92 @@
+package utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
+
+import com.mysql.jdbc.Connection;
+
+
+
+/*
+ * 获取数据库连接
+ * 释放资源
+ */
+public class DBUtils {
+
+	private static final Properties properties = new Properties();
+
+	static {
+		InputStream inputStream = DBUtils.class.getResourceAsStream("DB.properties");
+		
+		try {
+			properties.load(inputStream);
+
+			Class.forName(properties.getProperty("driver"));
+
+		} catch (ClassNotFoundException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	// 1.获取数据库连接
+	public static Connection getConnection() {
+		Connection connection = null;
+		try {
+
+			connection = (Connection) DriverManager.getConnection(properties.getProperty("url"),
+					properties.getProperty("username"), properties.getProperty("password"));
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+
+		}
+		return connection;
+	}
+
+	// 2.释放资源
+	public static void close(Connection connection, PreparedStatement prepareStatement) {
+		if (prepareStatement != null) {
+			try {
+				prepareStatement.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		if (connection != null) {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	// 2.释放资源
+	public static void closeAll(Connection connection,PreparedStatement preparedStatement,ResultSet resultSet) {
+		if (resultSet != null) {
+			try {
+				resultSet.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		if (preparedStatement != null) {
+			try {
+				preparedStatement.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		if (connection != null) {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
